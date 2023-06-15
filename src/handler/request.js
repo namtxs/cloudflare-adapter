ccba3b1931225746c5290304981a1323d216bd33
几句レし <namdevel@gmail.com>
2023-06-15 21:07:37 +0700

    init: create request handler

---
+++ src/handler/request.js
@@ -0,0 +1,16 @@
+export default function (req) {
+  req.headers = new Headers(req.headers);
+  req.json = () => {
+    return JSON.parse(req.body.toString());
+  };
+  req.arrayBuffer = () => {
+    return req.body;
+  };
+  req.text = () => {
+    return req.body.toString();
+  };
+  req.formData = () => {
+    return new URLSearchParams(req.body.toString());
+  };
+  return req;
+}
